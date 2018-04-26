3a71c359012324b44cd82e70ac343f7430353a84
JJ Ashcraft <jj@jjashcraft.com>
2018-04-25 22:41:42 -0400

    added get and post, able to retrieve and display quotes from MongoDB Sandbox.

--- server.js
+++ server.js
@@ -1,29 +1,36 @@
-const express = require('express');
+const express = require('express')
+const app = express()
 const bodyParser = require ('body-parser')
 const MongoClient = require('mongodb').MongoClient
-const app = express();
 
-app.use (bodyParser.urlencoded({extended: true}))
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-})
-
-app.post ('/quotes', (req, res) => {
-  db.collection('quotes').save(req.body, (err, result) => {
-    if (err) return console.log(err);
-    console.log('saved to database');
-    res.redirect('/')
-  })
-
-})
 
 var db
 
 MongoClient.connect('mongodb://josh:josh@ds247698.mlab.com:47698/onepositivethought', (err, client) => {
   if (err) return console.log(err)
   db = client.db('onepositivethought')
-  app.listen(3000, () => {
+  app.listen(process.env.PORT || 3000, () => {
     console.log('listening on 3000')
+  })
+})
+app.set('view engine', 'ejs')
+app.use (bodyParser.urlencoded({extended: true}))
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// })
+
+app.get('/', (req, res) => {
+  var cursor = db.collection('quotes').find().toArray((err, result) => {
+    console.log(result)
+    res.render('index.ejs', {quotes: result})
+  })
+})
+
+app.post ('/quotes', (req, res) => {
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
   })
 })
